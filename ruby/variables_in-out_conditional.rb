0ce7738831190b79ba1b53e823922f58800814f1
Aary Angre <87111984+ph4ntomgeek@users.noreply.github.com>
2023-07-27 12:05:06 +0530

    Create variables_in-out_conditional.rb

---
+++ ruby/variables_in-out_conditional.rb
@@ -0,0 +1,25 @@
+
+print "hello there"
+#no new line printed
+puts "Hello World"
+#puts will print to screen followed by a new line
+name = gets #stores input
+#gets.chomp() gets rid of new line in gets
+
+variable_one = "name"
+puts ("concatination can be done using" + variable_one)
+
+#data types
+string =  "data"
+numbers = 440.204
+boolean = true
+null = nil
+
+puts "use backslash to print \" "
+#methods of string: name.upcase(), name.downcase(), name.strip(), name.length(),name.include? "tecxt to  search", can be indexed like arrays
+puts string_name[0, 3] #prints from range 0 to 3 exclusiding 3, .index("a") will return index of a
+
+#number methods, abs, ceil, floor
+math.sqrt(36), #lots of such functions in math class
+puts 10/7 #gives 1, 
+puts 10/7.0 #gives all decimals
