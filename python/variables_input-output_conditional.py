e00a324a733711b87e44bdd53f12f1873229b2ea
Aary Angre <87111984+ph4ntomgeek@users.noreply.github.com>
2023-07-27 11:42:35 +0530

    Create variables_input-output_conditional.py

---
+++ python/variables_input-output_conditional.py
@@ -0,0 +1,29 @@
+#ensure that indentation is proper since python does not use brackets 
+
+#no need to declare datatype
+character = 'a'
+integer = 5
+float = 6.9
+boolean = true
+
+a = input();
+#is used for inputs
+
+print variable_name
+print "foo"
+
+if integer < 10:
+  print "less than 10"
+elif integer = 10:
+  print "less than 10"
+else:
+  print "greater than 10"
+
+switch={
+      1:'a',
+      2:'e',
+      3:'i',
+      4:'o',
+      5:'u'
+      }
+  #switch.get(num,"Invalid input") is useful to return stuff in functions
